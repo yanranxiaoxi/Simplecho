262e49aa3859e43ab9703a9e6787dd849a1d36bd
XiaoXi <admin@soraharu.com>
2024-08-31 17:34:22 +0800

    fix: 错误的添加类方式

--- assets/js/match-dark-mode.js
+++ assets/js/match-dark-mode.js
@@ -1,11 +1,11 @@
 const expiredTime = new Date()
 expiredTime.setTime(new Date().getTime() + 372 * 24 * 60 * 60 * 1000)
 if (matchMedia('(prefers-color-scheme: dark)').matches) {
-	document.getElementsByTagName('body').addClass('dark');
-	document.getElementsByTagName('body').removeClass('light');
+	document.getElementsByTagName('body')[0].classList.add('dark');
+	document.getElementsByTagName('body')[0].classList.remove('light');
 	document.cookie = 'latest-prefers-color-scheme=dark; expires=' + expiredTime.toUTCString();
 } else if (matchMedia('(prefers-color-scheme: light)').matches) {
-	document.getElementsByTagName('body').addClass('light');
-	document.getElementsByTagName('body').removeClass('dark');
+	document.getElementsByTagName('body')[0].classList.add('light');
+	document.getElementsByTagName('body')[0].classList.remove('dark');
 	document.cookie = 'latest-prefers-color-scheme=light; expires=' + expiredTime.toUTCString();
 }

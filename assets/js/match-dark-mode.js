318a4dbd0c40de7bcf156566b6c25847e3437937
XiaoXi <admin@soraharu.com>
2025-07-03 17:42:53 +0800

    fix: prettier error

--- assets/js/match-dark-mode.js
+++ assets/js/match-dark-mode.js
@@ -1,6 +1,6 @@
 function matchDarkMode() {
 	const expiredTime = new Date()
-	expiredTime.setTime(new Date().getTime() + 365 * 24 * 60 * 60 * 1000)
+	expiredTime.setTime(new Date().getTime() + 365 * 24 * 60 * 60 * 1000);
 	if (matchMedia('(prefers-color-scheme: dark)').matches) {
 		document.cookie = 'latest-prefers-color-scheme=dark; expires=' + expiredTime.toUTCString();
 		document.getElementsByTagName('body')[0].classList.add('dark');

0789ffe5bf9dd41045623441b984c897cb254bca
XiaoXi <admin@soraharu.com>
2025-07-03 15:29:26 +0800

    fix: 修复暗色主题颜色错误问题

--- assets/js/match-dark-mode.js
+++ assets/js/match-dark-mode.js
@@ -1,11 +1,7 @@
 const expiredTime = new Date()
-expiredTime.setTime(new Date().getTime() + 372 * 24 * 60 * 60 * 1000)
+expiredTime.setTime(new Date().getTime() + 365 * 24 * 60 * 60 * 1000)
 if (matchMedia('(prefers-color-scheme: dark)').matches) {
-	document.getElementsByTagName('body')[0].classList.add('dark');
-	document.getElementsByTagName('body')[0].classList.remove('light');
 	document.cookie = 'latest-prefers-color-scheme=dark; expires=' + expiredTime.toUTCString();
 } else if (matchMedia('(prefers-color-scheme: light)').matches) {
-	document.getElementsByTagName('body')[0].classList.add('light');
-	document.getElementsByTagName('body')[0].classList.remove('dark');
 	document.cookie = 'latest-prefers-color-scheme=light; expires=' + expiredTime.toUTCString();
 }
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * 
  * @package Simplecho
  * @author XiaoXi
- * @version 0.1.19
+ * @version 0.1.20
  * @link https://soraharu.com/
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
